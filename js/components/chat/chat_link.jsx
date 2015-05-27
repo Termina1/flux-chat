ba6e3ecea7d5db781f6ffacca1a9daba9bda171b
Termina1 <terminal2010@gmail.com>
2015-05-27 12:13:03 +0300

    styling

--- js/components/chat/chat_link.jsx
+++ js/components/chat/chat_link.jsx
@@ -29,7 +29,9 @@ export default class ChatLink extends React.Component {
           Component = OG;
           break
       }
-      cnt = <div className="b-chat-link--content"><Component onUpdate={this.props.onUpdate} params={this.props.link}/></div>;
+      cnt = <div className="b-chat-link--content">
+        <Component onUpdate={this.props.onUpdate} params={this.props.link}/>
+      </div>;
 
     }
     return (
